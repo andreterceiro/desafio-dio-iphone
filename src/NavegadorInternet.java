2724a614f0f259a2f703556ea0f46b029bb04edb
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-08-26 20:20:27 -0300

    interface NavegadorInternet

---
+++ src/NavegadorInternet.java
@@ -0,0 +1,16 @@
+interface NavegadorInternet {
+    /**
+     * Permite exibir uma página
+     */
+    public Boolean exibirPagina();
+    
+    /**
+     * Permite adicionar uma nova aba
+     */    
+    public Boolean adicionarNovaAba();
+    
+    /**
+     * Permite atualizar uma página
+     */
+    public Boolean atualizarPagina();
+}
